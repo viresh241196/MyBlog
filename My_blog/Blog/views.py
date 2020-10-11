5cf3fb14b0ea319da9cc8e6f5786025cb8a9c804
viresh <vireshrawool007@gmail.com>
2020-10-11 13:38:47 +0530

    changes in templates

--- My_blog/Blog/views.py
+++ My_blog/Blog/views.py
@@ -10,12 +10,12 @@ from django.contrib.auth.models import User
 def home(request):
     posts = Post.objects.order_by("-date")
     content = {'posts': posts}
-    return render(request, 'blog/home.html', content)
+    return render(request, 'Blog/home.html', content)
 
 
 def about(request):
     content = {'id': 'hello'}
-    return render(request, 'blog/about.html', content)
+    return render(request, 'Blog/about.html', content)
 
 
 def new_post(request):
@@ -31,23 +31,23 @@ def new_post(request):
 
     else:
         form = NewPostForm()
-        return render(request, 'blog/new_post.html', {'form': form})
+        return render(request, 'Blog/new_post.html', {'form': form})
 
 
 def detail_post(request, id):
     post = Post.objects.get(id=id)
-    return render(request, 'blog/detail_post.html', {'post': post})
+    return render(request, 'Blog/detail_post.html', {'post': post})
 
 
 def user_post(request, author_id):
     posts = Post.objects.filter(author_id=author_id).order_by("-date")
     context = {'posts': posts}
-    return render(request, 'blog/user_posts.html', context)
+    return render(request, 'Blog/user_posts.html', context)
 
 
-def update_post(request,id):
+def update_post(request, id):
     if request.method == "POST":
-        post_update_form = NewPostForm(request.POST,request.FILES,instance=Post.objects.get(id=id))
+        post_update_form = NewPostForm(request.POST, request.FILES, instance=Post.objects.get(id=id))
         if post_update_form.is_valid():
             post_update_form.save()
             messages.success(request, f"Changes has been applied")
